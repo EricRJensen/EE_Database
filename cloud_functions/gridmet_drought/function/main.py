660ad744796e9e2444a6eafcaec0a022c1db8c69
Eric Jensen <33233973+EricRJensen@users.noreply.github.com>
2024-01-16 09:17:21 -0700

    Updated GridMET Drought cloud function and cleaned up issue with GridMET

--- cloud_functions/gridmet_drought/function/main.py
+++ cloud_functions/gridmet_drought/function/main.py
@@ -9,6 +9,7 @@ logging.getLogger('googleapiclient').setLevel(logging.INFO)
 logging.getLogger('requests').setLevel(logging.INFO)
 logging.getLogger('urllib3').setLevel(logging.INFO)
 
+
 def handle_event(event):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
@@ -23,15 +24,15 @@ def handle_event(event):
 
 
 def run_export():
-    """Earth Engine export
+    """Earth Engine export function
     """
     # Define time period to export
     start_date = datetime.datetime(1980, 1, 1)
     end_date = datetime.datetime(2050, 1, 1)
 
     # Define input Image Collection
-    in_ic_name = 'GridMET'
-    in_ic_paths = ['IDAHO_EPSCOR/GRIDMET']
+    in_ic_name = 'GridMET_Drought'
+    in_ic_paths = ['GRIDMET/DROUGHT']
     in_ic = ee.ImageCollection(in_ic_paths[0])
     in_ic_res = ee.Number(in_ic.first().projection().nominalScale()).round().getInfo()
 
@@ -43,20 +44,15 @@ def run_export():
         in_fc, in_fc_path, in_fc_id, land_unit_long, land_unit_short, tile_scale, mask, mask_path = define_parameters(fc_type)
 
         # Define variable from Image Collection
-        var_dict = {'precip': {'units': 'mm'},
-                    'tmmn': {'units': 'degrees C'},
-                    'tmmx': {'units': 'degrees C'},
-                    'eto': {'units': 'mm'},
-                    'vpd': {'units': 'kPa'},
-                    'windspeed': {'units': 'm/s'},
-                    'srad': {'units': 'W/m^2'}}
+        var_dict = {'Long_Term_Drought_Blend': {'units': 'drought'},
+                    'Short_Term_Drought_Blend': {'units': 'drought'}}
         
         # Loop over variables
-        var_names = ['precip', 'tmmn', 'tmmx', 'eto', 'vpd', 'windspeed', 'srad']
+        var_names = ['Long_Term_Drought_Blend', 'Short_Term_Drought_Blend']
 
         for var_name in var_names:
 
-            var_type = 'Continuous'
+            var_type = 'Categorical'
             var_units = var_dict.get(var_name).get('units')
 
             out_path = f"projects/climate-engine-pro/assets/blm-database/{land_unit_short.replace('_', '').lower()}-{in_ic_name.replace('_', '').lower()}-{var_name.replace('_', '').lower()}"
@@ -89,7 +85,7 @@ def run_export():
                     properties['mask_path'] = 'None'
         
                 # Generate image to extact statistics from
-                in_i = preprocess_gm(in_ic_paths = in_ic_paths, var_name = properties.get('var_name'), date = date)
+                in_i = preprocess_gm_drought(in_ic_paths = in_ic_paths, var_name = properties.get('var_name'), date = date)
 
                 # Conditionally apply mask to images
                 if properties.get('mask_path') == 'None':
@@ -103,10 +99,10 @@ def run_export():
                 in_fc = ee.FeatureCollection(properties.get('in_fc_path'))
 
                 # Run function to get time-series statistics for input feature collection
-                out_fc = img_to_pts_continuous(in_i = in_i, in_fc = in_fc, tile_scale = properties.get('tile_scale'))
+                out_fc = img_to_pts_categorical(in_i = in_i, in_fc = in_fc, in_ic_name = in_ic_name, tile_scale = properties.get('tile_scale'))
 
                 # Convert centroid time-series to image collection time-series
-                out_i = pts_to_img_continuous(in_fc = out_fc)
+                out_i = pts_to_img_categorical(in_fc = out_fc, in_ic_name = in_ic_name)
 
                 # Create out region for export
                 out_region = out_fc.geometry().buffer(20)
@@ -141,39 +137,94 @@ def export_img(out_i, out_region, out_path, properties):
     task.start()
 
 
-def img_to_pts_continuous(in_i, in_fc, tile_scale):
+def img_to_pts_categorical(in_i, in_fc, in_ic_name, tile_scale):
     """
     :param in_i: e.g. Image for single date
     :param in_fc: e.g. ee.FeatureCollection(in_fc_path)
-    :return: Earth Engine image of pixels at the equator with bands for percentiles and mean
+    :return: Earth Engine Feature Collection of points at the equator with properties for histogram bins
     """
     # Cast input image to ee.Image
     img = ee.Image(in_i)
-    
+
+    # Need to further pre-process drought blends to be able to extract bins consistent with drought.gov
+    # There are no reducers that allow histogram bins with variable widths, so we have to put bins into categories to start
+    if in_ic_name == "GridMET_Drought":
+        img = img.where(img.lt(-2.0), 0)\
+            .where(img.lt(-1.5).And(img.gte(-2.0)), 1)\
+            .where(img.lt(-1.2).And(img.gte(-1.5)), 2)\
+            .where(img.lt(-0.7).And(img.gte(-1.2)), 3)\
+            .where(img.lt(-0.5).And(img.gte(-0.7)), 4)\
+            .where(img.lt(0.5).And(img.gte(-0.5)), 5)\
+            .where(img.lt(0.7).And(img.gte(0.5)), 6)\
+            .where(img.lt(1.2).And(img.gte(0.7)), 7)\
+            .where(img.lt(1.5).And(img.gte(1.2)), 8)\
+            .where(img.lt(2.0).And(img.gte(1.5)), 9)\
+            .where(img.gte(2.0), 10).toInt()
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10']
+    # Maintain original values for USDM
+    elif in_ic_name == "USDM":
+        img = img
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5']
+    # Maintain original values for MTBS
+    elif in_ic_name == "MTBS":
+        img = img
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6']
+
     # Get resolution of the image
     res = img.select(0).projection().nominalScale()
-
-    # Conditionally convert polygon to point if smaller than area of pixel
-    def smallpolygons_to_points(f):
-        
-        f = ee.Feature(f)
-        f = ee.Feature(ee.Algorithms.If(f.area(100).gte(res.pow(2).multiply(2)), f, f.centroid()))
-        return(f)
-    
-    in_fc = in_fc.map(smallpolygons_to_points)
     
     # Run reduce regions for allotments and select only the columns with reducers
-    img_rr = img.reduceRegions(collection = in_fc, reducer = ee.Reducer.percentile([5, 25, 50, 75, 95])\
-                                .combine(reducer2 = ee.Reducer.mean(), sharedInputs = True),\
+    img_rr = img.reduceRegions(collection = in_fc, reducer = ee.Reducer.frequencyHistogram(),\
                                 scale = res,\
-                                tileScale = tile_scale).select(['mean', 'p.*'])
+                                tileScale = tile_scale).select(['histogram'])
     
+    # Function to process histogram and key names to set as properties
+    def process_histogram(f):
+        # Cast function to feature
+        f = ee.Feature(f)
+        
+        # Get histogram
+        histogram = ee.Dictionary(f.get('histogram'))
+        
+        # Function to rename histogram keys 
+        def rename_histogram_keys(key):
+            key = ee.String(key).slice(0,2)
+            return(ee.String('c').cat(key))
+        
+        # Rename histogram
+        histogram = histogram.rename(histogram.keys(), histogram.keys().map(rename_histogram_keys))
+        
+        return(f.set(histogram))
+    
+    # Clean up histogram and set as properties
+    img_rr = img_rr.map(process_histogram).select(['c.*'])
+
+    # Add values of 0 for any histogram classes without values
+    def add_missing_props(f):
+        f = ee.Feature(f)
+        
+        # Get properties from reduceRegions call
+        f_props = f.propertyNames().remove("system:index")
+        
+        # Iterate over properties in reduceRegions call to identify missing properties
+        def get_missing_props(prop, classes):
+            classes_remove = ee.List(classes).remove(prop)
+            return(classes_remove)
+        
+        missing_props = ee.List(f_props.iterate(get_missing_props, classes))
+        
+        # Construct dictionary of missing_props: 0 to add as properties to each feature
+        missing_props_dict = ee.Dictionary.fromLists(missing_props, ee.List.repeat(0, missing_props.length()))
+        
+        return(f.set(missing_props_dict))
+    img_rr = img_rr.map(add_missing_props)
+
     # Get list of RR features
     img_rr_list = img_rr.toList(img_rr.size())
     
     # Get size of RR features
     img_rr_size = img_rr_list.size()
-
+    
     # Function to create feature collection next to equator after reduction
     def pts_to_equator_rr(i):
         
@@ -195,17 +246,52 @@ def img_to_pts_continuous(in_i, in_fc, tile_scale):
     return(equator_fc)
 
 
-def pts_to_img_continuous(in_fc):
-    """
-    :param in_fc: e.g. Output of .img_to_pts_continuous()
-    :param properties: e.g. {'land-unit': land_unit, 'in-fc-path': in_fc_path, "in-fc-id": in_fc_id, "in-ic-paths": in_ic_path, "var-type": var_type, "var-name": var_name}
-    :return: Earth Engine image of pixels at the equator with values for land unit ID
-    """
+def pts_to_img_categorical(in_fc, in_ic_name):
+    '''
+    :param in_fc: e.g. output of img_to_pts_categorical
+    :param in_ic_name: e.g. input image collection name for applying logic
+    :return: Earth Engine image of pixels at the equator with bands for histogram bins
+    '''
     # Cast to FeatureCollections
     fc = ee.FeatureCollection(in_fc)
-    
+
+    # Define classes for reclassification of properties
+    # Reclassify drought blends using schema below
+    # <-2.0 (D4) = c0
+    # -2.0--1.5 (D3) = c1
+    # -1.5--1.2 (D2) = c2
+    # -1.2--0.7 (D1) = c3
+    # -0.7--0.5 (D0) = c4
+    # -0.5-0.5 (Neutral) = c5
+    # 0.5-0.7 (W0) = c6
+    # 0.7-1.2 (W1) = c7
+    # 1.2-1.5 (W2) = c8
+    # 1.5-2.0 (W3) = c9
+    # >2.0 (W4) = c10
+    if in_ic_name == "GridMET_Drought":
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9', 'c10']
+    # USDM classes described below
+    # -1 Neutral or Wet = c0
+    # 0 Abnormal Dry (D0) = c1
+    # 1 Moderate Drought (D1) = c2
+    # 2 Severe Drought (D2) = c3
+    # 3 Extreme Drought (D3) = c4
+    # 4 Exceptional Drought (D4) = c5
+    elif in_ic_name == "USDM":
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5']
+    # MTBS classes described below
+    # 0 Background = c0
+    # 1 Unburned to low severity = c1
+    # 2 Low severity = c2
+    # 3 Moderate severity = c3
+    # 4 High severity = c4
+    # 5 Increased greenness = c5
+    # 6 Non-mapping area = c6
+    elif in_ic_name == "MTBS":
+        classes = ['c0', 'c1', 'c2', 'c3', 'c4', 'c5', 'c6']
+        
     # Get list of properties to iterate over for creating multiband image for each date
-    props = fc.first().propertyNames().remove('system:index')
+    props = ee.List(classes)
     
     # Function to generate image from stats stored in Feature Collection property
     def generate_stat_image(prop):
@@ -219,51 +305,92 @@ def pts_to_img_continuous(in_fc):
     return(img_mb)
 
 
-def preprocess_gm(in_ic_paths, var_name, date):
-    """Function to preprocess GridMET
+# Function to calculate short-term and long-term blends
+def preprocess_gm_drought(in_ic_paths, var_name, date):
+    """
     :param in_ic_paths: e.g. ['GRIDMET/DROUGHT'] or ['projects/rangeland-analysis-platform/vegetation-cover-v3']
     :param var_name: e.g. 'NDVI', 'long_term_drought_blend', 'tmmn'
     :param date: e.g. system:time_start in milliseconds since Unix epoch
     :return: Earth Engine time-series image with dates (YYYYMMDD) as bands
     """
-    # Get GridMET drought dates to match temporal cadence to
-    gm_drought = ee.ImageCollection("GRIDMET/DROUGHT").filter(ee.Filter.eq('system:time_start', date))
-    gm_drought_dates = gm_drought.aggregate_array('system:time_start')
-
-    # Read-in gridmet image collection
+    # Read-in gridmet drought image collection
     in_ic = ee.ImageCollection(in_ic_paths[0])
 
-    # Function to aggregate statistics over the day range from GridMET drought
-    def aggregate_over_dates(date):
+    def generate_gm_drought_imgs(img):
+
+        # Define property list
+        property_list = ["system:index", "system:time_start"]
         
-        # Filter for previous five days
-        date = ee.Date(date)
-        in_ic_aggregate = in_ic.filterDate(date.advance(-5, 'day'), date)
+        # Define preliminary variables for short-term blend calculation
+        stb_variable = "Short_Term_Drought_Blend"
+        stb_pdsi_img = img.select("pdsi")
+        stb_z_img = img.select("z")
+        stb_spi90d_img = img.select("spi90d")
+        stb_spi30d_img = img.select("spi30d")
+        
+        # Define weights for short-term blend calculation
+        stb_pdsi_coef = 0.2
+        stb_z_coef = 0.35
+        stb_spi90d_coef = 0.25
+        stb_spi30d_coef = 0.2
+        
+        # Calculate short-term blend
+        stblend = stb_pdsi_img.expression(
+            "b() * pdsi_coef / 2 + spi90d * spi90d_coef + spi30d * spi30d_coef + z * z_coef / 2",{
+                "spi90d": stb_spi90d_img, 
+                "spi30d": stb_spi30d_img, 
+                "z": stb_z_img, 
+                "pdsi_coef": stb_pdsi_coef,
+                "spi90d_coef": stb_spi90d_coef, 
+                "spi30d_coef": stb_spi30d_coef, 
+                "z_coef": stb_z_coef})
+        
+        # Define preliminary variables for long-term blend calculation
+        ltb_variable = "Long_Term_Drought_Blend"
+        ltb_pdsi_img = img.select("pdsi")
+        ltb_spi180d_img = img.select("spi180d")
+        ltb_spi1y_img = img.select("spi1y")
+        ltb_spi2y_img = img.select("spi2y")
+        ltb_spi5y_img = img.select("spi5y")
+        
+        # Define weights for long-term blend calculation
+        ltb_pdsi_coef = 0.35
+        ltb_spi180d_coef = 0.15
+        ltb_spi1y_coef = 0.2
+        ltb_spi2y_coef = 0.2
+        ltb_spi5y_coef = 0.1
+        
+        # Calculate short-term blend
+        ltblend = ltb_pdsi_img.expression(
+            "b() * pdsi_coef / 2 + spi180d* spi180d_coef + spi1y * spi1y_coef + spi2y * spi2y_coef + spi5y * spi5y_coef",{
+                "spi180d": ltb_spi180d_img, 
+                "spi1y": ltb_spi1y_img, 
+                "spi2y": ltb_spi2y_img, 
+                "spi5y": ltb_spi5y_img,
+                "spi180d_coef": ltb_spi180d_coef, 
+                "spi1y_coef": ltb_spi1y_coef, 
+                "spi2y_coef": ltb_spi2y_coef,
+                "spi5y_coef": ltb_spi5y_coef, 
+                "pdsi_coef": ltb_pdsi_coef})
+        return ltblend.addBands(stblend).select([0,1], [ltb_variable, stb_variable]).copyProperties(img, property_list)
     
-        # Aggregate variables over that time and convert temperature to celsius
-        pr_img = in_ic_aggregate.select('pr').reduce(ee.Reducer.sum()).rename(['precip'])
-        tmmn_img = in_ic_aggregate.select('tmmn').reduce(ee.Reducer.mean()).rename(['tmmn']).subtract(273.15)
-        tmmx_img = in_ic_aggregate.select('tmmx').reduce(ee.Reducer.mean()).rename(['tmmx']).subtract(273.15)
-        eto_img = in_ic_aggregate.select('eto').reduce(ee.Reducer.sum()).rename(['eto'])
-        vpd_img = in_ic_aggregate.select('vpd').reduce(ee.Reducer.mean()).rename(['vpd'])
-        wind_img = in_ic_aggregate.select('vs').reduce(ee.Reducer.mean()).rename(['windspeed'])
-        solar_img = in_ic_aggregate.select('srad').reduce(ee.Reducer.mean()).rename(['srad'])
+    # Map function to calculate drought blend
+    # Filter for dates without NAs for the long term blend
+    out_ic = in_ic.filter(ee.Filter.eq('system:time_start', date)).map(generate_gm_drought_imgs)
     
-        return(pr_img.addBands(tmmn_img).addBands(tmmx_img).addBands(eto_img).addBands(vpd_img).addBands(wind_img).addBands(solar_img)\
-                .set('system:time_start', date)\
-                .set('system:index', date.format('YYYYMMdd')))
-
-    # Apply aggregation function and convert to multiband image
-    out_ic = ee.ImageCollection(gm_drought_dates.map(aggregate_over_dates)).select(var_name)
+    # Convert Image Collection to multi-band image
     out_i = out_ic.toBands()
-
+    
+    # Select variable to serve as input
+    out_i = out_i.select(['[0-9]{8}_' + var_name])
+    
     # Bandnames must be an eight digit character string 'YYYYMMDD'. Annual data will be 'YYYY0101'.
     def replace_name(name):
         return ee.String(name).replace(var_name, '').replace('_', '')
     
-    # Finish cleaning input image and apply projection
-    out_i = out_i.rename(out_i.bandNames().map(replace_name)).setDefaultProjection(in_ic.first().projection())
-
+    # Finish cleaning input image
+    out_i = out_i.rename(out_i.bandNames().map(replace_name))
+    
     return(out_i)
 
 
